ee315b311acf8c8a341b0c3b9cd146f24df53b15
Arne Martin Aurlien <am@aurlien.net>
2014-04-30 16:22:33 +0200

    Major rewrite and simplification.

--- hamburger-basement.js
+++ hamburger-basement.js
@@ -5,147 +5,143 @@
         tc = tc || root.ReactTopcoat;
         var d = React.DOM;
 
+        // Header component
+        var topbar = React.createClass({
+            toggle: function() {
+                this.props.toggle();
+            },
+            render: function() {
+                return d.div(
+                    { className: 'hamburger-basement-topbar'},
+                    tc.navigationBar({
+                        title: this.props.title,
+                        leftButton: tc.iconButton({
+                            type: 'quiet',
+                            onClick: this.toggle,
+                            which: 'menu-stack'
+                        })
+                    })
+                );
+            }
+        });
+
         return React.createClass({
             getDefaultProps: function() {
                 return {
                     basement: d.div(null, 'I am basement'),
                     content: d.div(null, 'I am content'),
                     animationDuration: 0.4,
-                    basementWidth: window.innerWidth,
+                    basementWidth: 0, // 0 = set automatically
                     toggleDistanceDelta: 50,
-                    openOffset: 85,
+                    openOffset: 85
                 };
             },
             getInitialState: function() {
                 return {
-                    translate: 0,
-                    startingPosition: 0,
-                    touching: false,
-                    animate: false,
-                    open: false
+                    open: false, // Is the basement currently open?
+                    position: 0, // Position of the overlay
+                    touching: false, // Is touch interaction currently happening?
+                    currentBasementWidth: 0, // Current width of basement element, will be set after rendering and on resize
+                    touchOrigin: 0, // X position of last touchstart-event
+                    originalPosition: 0 // Original overlay position before touch started
                 };
-            },
-            enableAnimation: function() {
-                clearTimeout(this.deactivateanimationTimeout);
-                if (!this.state.animate) {
-                    this.setState({
-                        animate: true
-                    });
-                }
-            },
-            disableAnimation: function() {
-                clearTimeout(this.deactivateanimationTimeout);
-                if (this.state.animate) {
-                    this.setState({
-                        animate: false
-                    });
-                }
-            },
-            // Slide the overlay to a specific position
-            slideTo: function(translate, params) {
-                this.enableAnimation();
-                this.setState({
-                    touching: false,
-                    translate: translate,
-                    open: (params ? params.open : this.state.open)
-                });
-                var self = this;
-                this.deactivateanimationTimeout = setTimeout(function() {
-                    self.disableAnimation();
-                }, this.props.animationDuration * 1000);
-            },
-            open: function() {
-                this.slideTo(this.props.basementWidth - this.props.openOffset, { open: true });
-            },
-            close: function() {
-                this.slideTo(0, { open: false });
-            },
-            // Animate back to current open/closed state
-            goBack: function() {
-                if (this.state.open) {
-                    this.open();
-                } else {
-                    this.close();
-                }
             },
             // Toggle open/closed
             toggle: function() {
-                if (this.state.open) {
-                    this.close();
-                } else {
-                    this.open();
-                }
+                this.setState({ open: !this.state.open });
             },
             touchStart: function(e) {
                 this.setState({
-                    startingPosition: this.state.translate,
-                    startX: e.targetTouches[0].pageX,
-                    startY: e.targetTouches[0].pageY
+                    touching: true,
+                    touchOrigin: e.targetTouches[0].clientX,
+                    originalPosition: this.state.position
                 });
-                this.disableAnimation();
             },
             touchMove: function(e) {
-                this.setState({
-                    touching: true,
-                    deltaX: e.targetTouches[0].pageX - this.state.startX,
-                    deltaY: e.targetTouches[0].pageY - this.state.startY
-                });
-                if (Math.abs(this.state.deltaX) > Math.abs(this.state.deltaY) * 3) {
-                    e.preventDefault();
-                    if (this.state.deltaX < 0 && !this.state.open) {
-                        this.deltaX = 0;
-                    }
+                if (this.state.touching) {
+                    var newPos = Math.max(0, this.state.originalPosition + (e.targetTouches[0].clientX - this.state.touchOrigin));
                     this.setState({
-                        translate: this.state.deltaX + this.state.startingPosition
+                        position: newPos
                     });
                 }
             },
             touchEnd: function() {
                 if (this.state.touching) {
-                    if (Math.abs(this.state.deltaX) > this.props.toggleDistanceDelta) {
-                        this.toggle();
+                    var shouldToggle = Math.abs(this.state.originalPosition - this.state.position) > this.props.toggleDistanceDelta;
+                    if (shouldToggle) {
+                        this.setState({
+                            touching: false,
+                            open: !this.state.open
+                        });
                     } else {
-                        this.goBack();
+                        this.setState({
+                            touching: false,
+                            position: this.state.originalPosition
+                        });
                     }
                 }
             },
+            // Calculate overlay position when the basement is open
+            openPosition: function() {
+                if (this.basementWidth > 0) {
+                    return this.props.basementWidth - this.props.openOffset;
+                } else {
+                    return this.state.currentBasementWidth - this.props.openOffset;
+                }
+            },
+            resize: function() {
+                this.setState({ currentBasementWidth: this.refs.basement.getDOMNode().offsetWidth });
+                if (this.state.open) {
+                    this.setState({ position: this.openPosition() });
+                }
+            },
+            componentDidUpdate: function(prevProps, prevState) {
+                // Check to see if we should toggle the overlay position
+                if (prevState.open !== this.state.open) {
+                    this.setState({ position: this.state.open ? this.openPosition() : 0 });
+                }
+            },
+            componentDidMount: function() {
+                // Make sure resizing the window is supported
+                // This should also handle portrait/landscape switching on mobile
+                window.addEventListener('resize', this.resize);
+                this.setState({ currentBasementWidth: this.refs.basement.getDOMNode().offsetWidth });
+            },
+            componentWillUnmount: function() {
+                window.removeEventListener('resize', this.resize);
+            },
             render: function() {
-                console.log('rendring');
                 return d.div(
                     { className: 'hamburger-basement' },
                     d.div(
-                        { className: 'hamburger-basement-basement' },
+                        {
+                            className: 'hamburger-basement-basement',
+                            ref: 'basement'
+                        },
                         this.props.basement
                     ),
                     d.div(
                         {
                             className: 'hamburger-basement-main',
                             style: {
-                                '-webkit-transform': 'translate3d(' + this.state.translate + 'px, 0, 0)',
-                                '-moz-transform': 'translateX(' + this.state.translate + 'px)',
-                                '-o-transform': 'translateX(' + this.state.translate + 'px)',
-                                'transform': 'translateX(' + this.state.translate + 'px)',
-                                '-webkit-transition': (this.state.animate ? '-webkit-transform ' + this.props.animationDuration + 's ease-out' : 'none'),
-                                '-moz-transition': (this.state.animate ? '-moz-transform ' + this.props.animationDuration + 's ease-out' : 'none'),
-                                '-o-transition': (this.state.animate ? '-o-transform ' + this.props.animationDuration + 's ease-out' : 'none'),
-                                'transition': (this.state.animate ? 'transform ' + this.props.animationDuration + 's ease-out' : 'none')
+                                '-webkit-transform': 'translate3d(' + this.state.position + 'px, 0, 0)',
+                                '-moz-transform': 'translateX(' + this.state.position + 'px)',
+                                '-o-transform': 'translateX(' + this.state.position + 'px)',
+                                'transform': 'translateX(' + this.state.position + 'px)',
+                                '-webkit-transition': (this.state.touching ? 'none' : '-webkit-transform ' + this.props.animationDuration + 's ease-out'),
+                                '-moz-transition': (this.state.touching ? 'none' : '-moz-transform ' + this.props.animationDuration + 's ease-out'),
+                                '-o-transition': (this.state.touching ? 'none' : '-o-transform ' + this.props.animationDuration + 's ease-out'),
+                                'transition': (this.state.touching ? 'none' : 'transform ' + this.props.animationDuration + 's ease-out')
                             },
                             onTouchStart: this.touchStart,
                             onTouchMove: this.touchMove,
                             onTouchEnd: this.touchEnd,
                             onTouchCancel: this.touchEnd
                         },
-                        d.div(
-                            { className: 'hamburger-basement-topbar'},
-                            tc.navigationBar({
-                                title: this.props.title,
-                                leftButton: tc.iconButton({
-                                    type: 'quiet',
-                                    onClick: this.toggle,
-                                    which: 'menu-stack'
-                                })
-                            })
-                        ),
+                        topbar({
+                            title: this.props.title,
+                            toggle: this.toggle
+                        }),
                         d.div(
                             { className: 'hamburger-basement-content'},
                             this.props.content
